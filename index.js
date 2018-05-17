e7e294b887e3beceacaac16f2e7a385241d12a00
Philip Gregorius <philip@reges.se>
2018-05-17 15:21:48 +0200

    v2

--- index.js
+++ index.js
@@ -1,47 +1,107 @@
-const path = require('path');
-const spawnSync = require('child_process').spawnSync;
+const fs = require("fs");
+const path = require("path");
+const spawnSync = require("child_process").spawnSync;
 
-const mapToProcess = parameters => {
-  Object.keys(parameters).forEach(key => {
-    if (!process.env[key]) {
-      process.env[key] = parameters[key];
-    }
+const spawnSSMProcess = (region, parametersToLoad) => {
+  const result = spawnSync("node", [__dirname + "/syncssm"], {
+    input: [region, ...parametersToLoad].join(","),
+    maxBuffer: 4000000
   });
-};
+
+  const params = JSON.parse(result.stdout.toString());
+
+  if (!params) {
+    throw new Error("Could not load params");
+  }
+
+  return params;
+}
 
 module.exports = (config = {}) => {
   try {
-    if (config.origin === 'aws') {
-      if (!config.parameters || !Array.isArray(config.parameters)) {
-        throw new TypeError('parameters are required for remote config');
+    let loadFromFile = false;
+    let secrets = {};
+    let params;
+    const configurationsToLoad = [];
+    const currentEnvironment = process.env.NODE_ENV;
+
+    if (!{}.hasOwnProperty.call(config, "useLocal")) config.useLocal = true;
+    if (!{}.hasOwnProperty.call(config, "region")) config.region = "eu-west-1";
+    if (config.useLocal) loadFromFile = fs.existsSync("./.env.json");
+
+    if ({}.hasOwnProperty.call(config, "default"))
+      configurationsToLoad.push("default");
+    if (
+      {}.hasOwnProperty.call(config, "environments") &&
+      {}.hasOwnProperty.call(config.environments, currentEnvironment)
+    )
+      configurationsToLoad.push(currentEnvironment);
+
+    if (loadFromFile) {
+      const parametersToLoad = [];
+
+      for (let configuration of configurationsToLoad) {
+        if (
+          configuration === "default" &&
+          {}.hasOwnProperty.call(config.default, "parameters")
+        ) {
+          parametersToLoad.push(...config.default.parameters);
+        } else if (
+          {}.hasOwnProperty.call(
+            config.environments[currentEnvironment],
+            "parameters"
+          )
+        ) {
+          parametersToLoad.push(
+            ...config.environments[currentEnvironment].parameters
+          );
+        }
       }
 
-      if (!config.region) {
-        throw new TypeError('region is required for remote config');
-      }
-
-      const result = spawnSync('node', [__dirname + '/syncssm'], {
-        input: [config.region, ...config.parameters].join(','),
-        maxBuffer: 4000000
-      });
-
-      const params = JSON.parse(result.stdout.toString());
-
-      if (!params) {
-        throw new Error('Could not load params');
-      }
-
-      if (params) {
-        mapToProcess(
-          params.reduce((state, next) => {
-            state[next.Name] = next.Value;
-            return state;
-          }, {})
-        );
+      const configFile = require("./.env.json");
+      for (let param of parametersToLoad) {
+        if (configFile[param.key]) {
+          process.env[param.name] = configFile[param.key];
+        }
       }
     } else {
-      const env = require(path.resolve(process.cwd(), '.env.json'));
-      mapToProcess(env);
+      const parametersToLoad = [];
+
+      for (let configuration of configurationsToLoad) {
+        if (
+          configuration === "default" &&
+          {}.hasOwnProperty.call(config.default, "parameters")
+        ) {
+          let region;
+          if (!{}.hasOwnProperty.call(config.default, "region") && !config.region) region = "eu-west-1";
+          if(!region) region = config.region;
+
+          const parameters = spawnSSMProcess(region, config.default.parameters.map(p => p.key));          
+          for(let param of parameters) {
+            const specParam = config.default.parameters.find(p => p.key === param.Name);
+            if(specParam) {
+              process.env[specParam.name] = param.Value; 
+            }
+          }
+        } else if (
+          {}.hasOwnProperty.call(
+            config.environments[currentEnvironment],
+            "parameters"
+          )
+        ) {
+          let region;
+          if (!{}.hasOwnProperty.call(config.environments[currentEnvironment], "region") && !config.region) region = "eu-west-1";
+          if(!region) region = config.region;
+
+          const parameters = spawnSSMProcess(region, config.environments[currentEnvironment].parameters.map(p => p.key));     
+          for(let param of parameters) {
+            const specParam = config.environments[currentEnvironment].parameters.find(p => p.key === param.Name);
+            if(specParam) {
+              process.env[specParam.name] = param.Value; 
+            }
+          }
+        }
+      }
     }
   } catch (err) {
     console.log(err);
--- syncssm.js
+++ syncssm.js
@@ -1,30 +1,55 @@
-const AWS = require('aws-sdk');
-const readline = require('readline');
+const AWS = require("aws-sdk");
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.on('line', input => {
+const chunkArray = (arr, chunk_size) => {
+  let index = 0;
+  let arrayLength = arr.length;
+  let tempArray = [];
+
+  for (index = 0; index < arrayLength; index += chunk_size) {
+    myChunk = arr.slice(index, index + chunk_size);
+    tempArray.push(myChunk);
+  }
+
+  return tempArray;
+};
+
+const getParametersAsync = (ssm, config) =>
+  new Promise((resolve, reject) => {
+    ssm.getParameters(config, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data.Parameters);
+    });
+  });
+
+rl.on("line", async input => {
   try {
-    let args = input.split(',');
+    let args = input.split(",");
     let region = args.shift();
 
     AWS.config.update({ region: region });
-    const ssm = new AWS.SSM();
 
-    ssm.getParameters(
-      {
-        Names: [...args],
-        WithDecryption: true
-      },
-      (err, data) => {
-        if (data) {
-          console.log(JSON.stringify(data.Parameters));
-        }
-      }
-    );
+    const ssm = new AWS.SSM();
+    const paramChunks = chunkArray(args, 10);
+    const loadedParameters = [];
+
+    for (let chunk of paramChunks) {
+      loadedParameters.push(
+        ...(await getParametersAsync(ssm, {
+          Names: [...chunk],
+          WithDecryption: true
+        }))
+      );
+    }
+    
+    console.log(JSON.stringify(loadedParameters));
   } catch (err) {
     console.log(undefined);
   } finally {
